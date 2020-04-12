557076185dc14ecc278805caa191ce59a4365ad4
Cleiston <cleistonrs@gmail.com>
2020-04-12 10:31:01 -0300

    Finished bar chart

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
+import './styles.css';
 
 export default function BarChart({country, countryInfo}) {
 
@@ -10,18 +11,18 @@ const data = {
     datasets: [
       {
         label: country,
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
+        backgroundColor: 'rgba(0,120,132,0.2)',
+        borderColor: 'rgba(0,120,132,1)',
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
+        hoverBackgroundColor: 'rgba(0,120,132,0.4)',
+        hoverBorderColor: 'rgba(0,120,132,1)',
         data: [confirmed, recovered, deaths]
       }
     ]
   };
 
     return (
-    <div>
+    <div className="container-barchart">
         <Bar
         data={data}
         width={100}
